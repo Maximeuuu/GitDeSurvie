0afbc85b7773ecab31a8ec77ce67ee7ffa0d3aa3
Maximeuuu <maxime.lemoine374@gmail.com>
2024-01-13 15:22:23 +0100

    déplacement des dossiers

--- TP3/AthosCoin.java
+++
@@ -1,8 +0,0 @@
-public class AthosCoin extends Cryptomonnaie
-{
-	public AthosCoin()
-	{
-		super("ATH", 49.3);
-	}
-}
-
--- TP3/PorthosCoin.java
+++
@@ -1,6 +0,0 @@
-public class PorthosCoin extends Cryptomonnaie{
-    public PorthosCoin(){
-        super("PORTHOS", 500000);
-    }
-}
-
